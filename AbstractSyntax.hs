6b309c52b937a971ac95f0c45d17288778bbb816
gregr <greg.weiqi@gmail.com>
2012-03-04 17:41:37 -0800

    AbstractSyntax: I have no idea what I am doing

--- AbstractSyntax.hs
+++ AbstractSyntax.hs
@@ -1,5 +1,8 @@
 module AbstractSyntax where
 
+import Data.Map as M qualified
+import Data.Set as S qualified
+
 type Name = String
 type Index = Integer
 type Binding val = (Name, val)
@@ -40,3 +43,46 @@ data Type' ty = TyVar Name
 -- Cost (time, space, etc.)
 
 data Value' value = ty subtag datums
+
+
+-- new idea for stratified kind/type/value/repr via unified representation
+-- todo: maybe start without refinements to experiment with something simpler
+-- todo: environments need to carry constraints (such as Int <: x, which aren't natural binding forms like x <: Int)
+-- todo: free vs. bound 'type' variables; renaming only bound vars during environment linking? such as the 'a' in 'a -> List a'
+--      might be better to call bound vars non-free, since they may not have explicit bounds
+--      free vars are those directly associated with function params, or other free term names within a term body
+
+type VSet = S.Set Value
+type RSet = S.Set RefinedDatum
+
+type VIntersection = VSet
+
+data VUnion = PosSet RSet -- empty is bottom
+            | NegSet RSet -- empty is top
+
+data Variance = Covariant | Contravariant | Invariant
+-- instead of variance, negative types? hmm that seems gross, and doesn't allow for invariance
+
+data Typing = Typing Value Env
+
+data ConstrArrow = ConstrArrow [(Variance, Value)] Datum
+
+-- todo: these need to carry around environments? do datums? maybe not since they can always be linked to the current context
+-- todo: these and datums also need to specify implicit args (constraint satisfiers)
+--    envs should identify these components that provide constraint satisfaction, so these can be propagated upon lookup
+data Constr = Constr { tag :: Tag, constrArrow :: ConstrArrow, constrEnv :: Env }
+  -- unifying paramTypes that are vars should instantiate new vars in Env
+
+data Datum = Datum Constr [Value] -- type of Datum is its constructor's [partially] evaluated arrow, given these args
+           | Infinity Integer -- allow for endless stratification, type is greater Infinity
+
+data RefinedDatum = RefinedDatum Datum RSet
+
+data Value = VUnion VUnion -- todo: is this necessary given Env's implicit union? maybe for efficiency
+           | VVar Name
+
+type BindingMap = M.Map Name VIntersection
+
+-- todo: more efficient representation for variable lookup
+data Env = Env [BindingMap] -- represent unions of entangled vars / universes
+
