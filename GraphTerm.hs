4097336f686f57582f7273d3177396d71345f42c
gregr <greg.weiqi@gmail.com>
2013-04-05 09:56:47 -0400

    Separate values; use newtype; cleanup

--- GraphTerm.hs
+++ GraphTerm.hs
@@ -8,89 +8,52 @@ import Control.Monad.State
 {-import Control.Monad.Error-}
 {-import Control.Monad.Identity-}
 
+-- TODO: will probably need this later for ad-hoc evaluation
+{-bn_lift idx target =-}
+  {-if idx >= target then idx + 1 else idx-}
+{-bn_lower idx target =-}
+  {-if idx == target then Nothing else-}
+    {-Just (if idx > target then idx - 1 else idx)-}
+{--- TODO: lift to make room for a new lambda binding; does this operation make sense on bn?-}
+{--- bn_abstract ... =-}
+{-bn_substitute idx target val =-}
+  {-case bn_lower idx target of-}
+    {-Nothing -> val-}
+    {-Just bname -> Var bname-}
+{--- TODO: term wrappers?-}
+{-term_substitute rtsub term target val = tsub term-}
+  {-where-}
+    {-tsub (Var bname) = bn_substitute bname target val-}
+    {-tsub (Lam body) = Lam $ rtsub body (target + 1) val-}
+    {-tsub (App proc arg) = App (rtsub proc target val) (rtsub arg target val)-}
+
+-- coinductive data dealt with by change in evaluation strategy to normal order
+--   no need for explicit delay/force syntax (reserved for lower level languages?)
+--   functions parameters are analyzed to see which can endanger termination
+--   when termination is jeopardized by coinductively defined data, evaluation of
+--   applications switches to normal order, thunking/delaying expressions
+--     strictness analysis done to figure out when to force thunks before case scrutiny?
+
+-- function cost analysis: per iteration/step, full undelayed process (may be infinite), parameterized by function parameters
+-- possible nontermination as effect
+-- termination analysis indicates dependency on the finiteness of certain parameters, maybe just in a sentence using AND and OR; failing this dependency indicates the function application needs to be delayed until forced (recursive calls will naturally do the same)
+--   when operating dynamically, finding "delayed" expressions in these positions triggers further delay
+
 type Address = Int
 type Name = Int
 
-{-newtype FreeName = FreeName Int-}
-  {-deriving (Show, Eq, Ord)-}
-{-newtype BoundName = BoundName Int-}
-  {-deriving (Show, Eq, Ord)-}
-bn_lift idx target =
-  if idx >= target then idx + 1 else idx
-bn_lower idx target =
-  if idx == target then Nothing else
-    Just (if idx > target then idx - 1 else idx)
--- TODO: lift to make room for a new lambda binding; does this operation make sense on bn?
--- bn_abstract ... =
-bn_substitute idx target val =
-  case bn_lower idx target of
-    Nothing -> val
-    Just bname -> Var bname
-
-data TermT term = Var Name | Lam term | App term term
+data ValueT term = Lam term -- | Tuple [Name] -- TODO: constructors must be in ANF
+  deriving (Show, Eq)
+data TermT term = Value (ValueT term) | Var Name | LetRec [(Name, term)] term | App term term
   deriving (Show, Eq)
 
 -- TODO: where to track open binders?
 type Labeled term = ((), term)
 type Addressed term = Either Address term
-data FinalTerm = FinalTerm (Addressed (Labeled (TermT FinalTerm)))
--- TODO: term wrappers?
-term_substitute rtsub term target val = tsub term
-  where
-    tsub (Var bname) = bn_substitute bname target val
-    tsub (Lam body) = Lam $ rtsub body (target + 1) val
-    tsub (App proc arg) = App (rtsub proc target val) (rtsub arg target val)
+newtype ALTerm = ALTerm (Addressed (Labeled (TermT ALTerm)))
+  deriving (Show, Eq)
 
-{-data Graph term = Graph{gr_index :: M.Map FreeName term, gr_next :: FreeName}-}
-  {-deriving (Show, Eq)-}
-{-graph_new = Graph{gr_index = M.empty, gr_next = FreeName 0}-}
-{-graph_insert term = do-}
-  {-graph <- get-}
-  {-let-}
-    {-index = gr_index graph-}
-    {-fnext = gr_next graph-}
-    {-FreeName next = fnext-}
-    {-index' = M.insert fnext term index-}
-  {-put $ Graph{gr_index = index', gr_next = FreeName (next + 1)}-}
-  {-return fnext-}
-
-{-type GraphTerm = TermT FreeName-}
-{-data BindingGraphTerm = BindingGraphTerm{bnd_term :: GraphTerm, bnd_deps :: S.Set BoundName}-}
-{-type BindingGraph = Graph BindingGraphTerm-}
-
-
-
-
-
-{-data Substitution term = Offset Int | Bind term-}
-{---newtype Env term = Env (M.Map BoundName term)-}
-{-newtype Env term = Env {env_subs :: [Substitution term], env_bindcount :: Int}-}
-{-env_make bindings = Env {env_subs = subs, env_bindcount = length bindings}-}
-  {-where subs =-}
-{-type GraphEnv = Env FreeName-}
-
--- TODO: must rename/lift affected bound names while substituting
--- must increment env bound names when entering lambda bodies
--- gsub (bname, fname) bodyname graph =
-
--- maybe later; keep things simple
---newtype Binder = Binder {bnd_name :: Name, bnd_deps :: S.Set BoundName}
---type BoundTerm = TermT Binder Binder
---newtype BoundTerm = BoundTerm {bt_term :: TermT Name Name, bt_deps :: S.Set BoundName}
---type BoundGraph = Graph BoundTerm
-
---type FreeTerm = TermT FreeName Binder
---type FreeGraph = Graph FreeTerm
-
---type Env = (FreeGraph, BoundGraph)
-
-{-apply lam arg = ...-}
-
--- eval term or eval name?
-{-eval fname graph = evalf fterm env-}
-  {-where-}
-    {-evalf term@(Lam bterm) env = term-}
-    {-evalf (App tlam targ) env = apply lam arg-}
-      {-where lam = evalf tlam env-}
-            {-arg = evalf targ env-}
-    --evalb
+newtype SimpleTerm = SimpleTerm (TermT SimpleTerm)
+  deriving (Show, Eq)
+-- TODO: with zipper context?
+--simple_eval
