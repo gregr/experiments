aa45135c614804a00f5979d79706a16cb06cd133
gregr <greg.weiqi@gmail.com>
2013-09-02 16:40:31 -0400

    Add stand-alone program to interpret terms

---
+++ supercompilation/cog/run.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(require readline)
+(require "util.rkt")
+(require "base.rkt")
+
+(define eval-port (make-parameter denote-eval-port))
+(define program-source (make-parameter (current-input-port)))
+
+(command-line
+ #:once-each
+ (("-f" "--file") filename "Read program terms from file"
+                  (program-source (open-input-file filename)))
+ #:once-any
+ (("-s" "--small-step") "Choose small-step interpretation"
+                        (eval-port step-eval-port))
+ (("-d" "--denotational") "Choose denotational interpretation"
+                          (eval-port denote-eval-port)))
+
+(match ((eval-port) (program-source))
+  ((left err) (displayln err))
+  ((right _) (void)))
