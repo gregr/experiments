42f9dc2246ec8bf91d1d58b2128a30fd4fb17a1e
gregr <greg.weiqi@gmail.com>
2013-11-26 00:19:05 -0500

    Add std-1 program linker

--- supercompilation/cog/base.rkt
+++ supercompilation/cog/base.rkt
@@ -970,3 +970,12 @@
 
 (match-define (cons lam-wrap (cons lam-unwrap (cons uno-1 std-1-input)))
   (map value std-0-output))
+
+(define (std-1 prog)
+  (let ((proc (right-x (parse-1 penv-init-1
+          `(lam (sym? lam? bit? uno? pair?
+                 sym-eq? 0b 1b pair pair-access
+                 produce error gen-sym)
+              ,prog)))))
+    (foldl (lambda (arg proc) (new-lam-apply-1 proc arg)) proc
+           std-1-input)))
