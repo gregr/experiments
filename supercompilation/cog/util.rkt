a10b2445ee04843ea833e70358d6d0e158c505c2
gregr <greg.weiqi@gmail.com>
2014-05-17 19:00:01 -0400

    Use gregr-misc/record

--- supercompilation/cog/util.rkt
+++ supercompilation/cog/util.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require gregr-misc/record)
 (require racket/stxparam)
 (provide (all-defined-out))
 
@@ -7,68 +8,6 @@
 (define (list-set xs idx val)
   (let-values (((start end) (split-at xs idx)))
               (append start (cons val (cdr end)))))
-
-; record syntax
-(require (for-syntax racket/list))
-
-(define-for-syntax (identifier-with-? ident)
-  (datum->syntax
-    ident
-    (string->symbol
-      (string-append (symbol->string (syntax->datum ident))
-                     "?"))))
-
-(define-syntax (record-hash stx)
-  (syntax-case stx ()
-    ((_ field ...)
-     (let ((kvs (flatten (map syntax->list
-                              (syntax->list #'(('field field) ...))))))
-       #`(hash #,@kvs)))))
-
-(define-syntax record-struct
-  (syntax-rules ()
-    ((_ name (field ...) struct-rest ...)
-     (struct name (field ...) #:transparent
-      #:methods gen:dict
-      ((define (dict-ref rec . rest)
-         (match rec
-           ((name field ...)
-            (apply hash-ref
-                   (cons (record-hash field ...) rest)))))
-       (define (dict-set rec key val)
-         (match rec
-           ((name field ...)
-            (let ((temp (hash-set (record-hash field ...) key val)))
-              (name (hash-ref temp 'field) ...)))))
-       (define (dict-iterate-first rec)
-         (if (empty? '(field ...)) #f 0))
-       (define (dict-iterate-next rec pos)
-         (let ((next (+ pos 1)))
-           (if (< next (length '(field ...))) next #f)))
-       (define (dict-iterate-key rec pos)
-         (list-ref '(field ...) pos))
-       (define (dict-iterate-value rec pos)
-         (match rec
-           ((name field ...) (list-ref (list field ...) pos))))
-       (define (dict-count rec)
-         (length '(field ...))))
-      struct-rest ...))))
-
-(define-syntax record
-  (syntax-rules ()
-    ((_ name field ...) (record-struct name (field ...)))))
-
-(define-syntax (records stx)
-  (syntax-case stx ()
-    ((_ name (rname rfield ...) ...)
-     #`(begin
-         (define (#,(identifier-with-? #'name) datum)
-           (or
-             #,@(map
-                  (lambda (ident)
-                    (list (identifier-with-? ident) #'datum))
-                  (syntax->list #'(rname ...)))))
-         (record rname rfield ...) ...))))
 
 ; cursors
 (define (ref+set datum)
