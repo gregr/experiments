b8ec0784886f1e51998ac83303be1ce464e905ef
gregr <greg.weiqi@gmail.com>
2014-05-17 19:48:38 -0400

    Use gregr-misc/cursor

--- supercompilation/cog/util.rkt
+++ supercompilation/cog/util.rkt
@@ -1,71 +1,12 @@
 #lang racket
 (provide (all-defined-out))
 
+(require gregr-misc/cursor)
 (require gregr-misc/list)
 (require gregr-misc/record)
 (require racket/stxparam)
 
 (define (pretty-string x) (call-with-output-string (curry pretty-print x)))
-
-; cursors
-(define (ref+set datum)
-  (cond
-    ((list? datum) (list list-ref list-set))
-    ((dict? datum) (list dict-ref dict-set))))
-(record cursor focus trail ancestors)
-(define (cursor-new datum) (cursor datum '() '()))
-(define (cursor-refocus cur new-focus) (dict-set cur 'focus new-focus))
-(define (cursor-ascend cur)
-  (match cur
-    ((cursor focus (cons key keys) (cons parent ancestors))
-     (match-let* ((`(,_ ,p-set) (ref+set parent))
-                  (new-focus (p-set parent key focus)))
-       (cursor new-focus keys ancestors)))))
-(define (cursor-ascend-to cur-src cur-tgt)
-  (for/fold ((cur cur-src))
-            ((idx (in-range (- (length (cursor-trail cur-src))
-                               (length (cursor-trail cur-tgt))))))
-    (cursor-ascend cur)))
-(define (cursor-ascend* cur) (cursor-ascend-to cur (cursor-new '())))
-(define (cursor-descend cur key)
-  (match cur
-    ((cursor focus keys ancestors)
-     (match-let* ((`(,p-ref ,_) (ref+set focus))
-                  (new-focus (p-ref focus key)))
-       (cursor new-focus (cons key keys) (cons focus ancestors))))))
-(define (cursor-descend* cur keys)
-  (foldl (flip cursor-descend) cur keys))
-
-; cursor notation
-(define :o (curry apply append))
-(define ::^
-  (case-lambda
-    ((cur) (cursor-ascend cur))
-    ((cur-src cur-tgt) (cursor-ascend-to cur-src cur-tgt))))
-(define (::^. cur-src cur-tgt)
-  (cursor-focus (cursor-ascend-to cur-src cur-tgt)))
-(define ::^* cursor-ascend*)
-(define ::^*. (compose1 cursor-focus cursor-ascend*))
-(define (::@* cur paths) (cursor-descend* cur (:o paths)))
-(define (::@ cur . paths) (::@* cur paths))
-(define (::. cur . paths) (cursor-focus (::@* cur paths)))
-(define (::= cur val . paths)
-  (cursor-ascend-to (cursor-refocus (::@* cur paths) val)
-                    cur))
-(define (::~ cur trans . paths)
-  (let ((cur-next (::@* cur paths)))
-    (cursor-ascend-to
-      (cursor-refocus cur-next (trans (cursor-focus cur-next)))
-      cur)))
-
-; lens-like operators
-(define (:. src . paths)       (::. (cursor-new src) (:o paths)))
-(define (:= src val . paths)   (::. (::= (cursor-new src) val (:o paths))))
-(define (:~ src trans . paths) (::. (::~ (cursor-new src) trans (:o paths))))
-(define (:.* src . path)       (:. src path))
-(define (:=* src val . path)   (:= src val path))
-(define (:~* src trans . path) (:~ src trans path))
-
 
 (record monad pure bind)
 
