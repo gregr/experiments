92e4eff4b616c87e0d1ff28100effb72227cab31
gregr <greg.weiqi@gmail.com>
2014-07-01 22:25:19 -0400

    Define finer-grained operational step

--- supercompilation/cog/semantics-operational.rkt
+++ supercompilation/cog/semantics-operational.rkt
@@ -14,25 +14,55 @@
   (((pair-access) (bit (b-1)) (pair p0 p1)) (just (value p1)))
   ((_             _           _)            (nothing)))
 
+(define (step-continuation-downward cterm)
+  (match (::. cterm)
+    ((value _)            (nothing))
+    ((produce tm)         (step-continuation-downward (::@ cterm '(tm))))
+    ((subst sub tm)       (just cterm))
+    ((action-2 act t0 t1)
+     (define (sub-cont choice)
+       (step-continuation-downward (::@ cterm (list choice))))
+     (maybe-or (sub-cont 't0) (sub-cont 't1) (just cterm)))))
+
+(define (step-continuation-upward-with key cterm)
+  (match* (key (::. cterm))
+    (('t0 (action-2 act t0 t1)) (::@ cterm '(t1)))
+    ((_   _)                    (step-continuation-upward cterm))))
+
+(define (step-continuation-upward cterm)
+  (match (cursor-trail cterm)
+    ('()             (nothing))
+    ((cons key keys)
+     (step-continuation-upward-with key (::^ cterm)))))
+
+(define (step-continuation cterm)
+  (maybe-or
+    (step-continuation-downward cterm)
+    (begin/with-monad
+      maybe-monad
+      upward <- (step-continuation-upward cterm)
+      (step-continuation upward))))
+
+(define (step-execute cterm)
+  (let ((term (::. cterm)))
+    (match term
+      ((value _)      (left (format "cannot execute value: ~v" term)))
+      ((produce _)    (left (format "cannot execute produce ~v" term)))
+      ((subst sub tm) (right (::= cterm (substitute-explicit sub tm))))
+      ((action-2 act (value v0) (value v1))
+       (either-map
+         (curry ::= cterm)
+         (maybe->either (format "cannot execute stuck term: ~v" term)
+                        (execute-action-2-explicit act v0 v1)))))))
+
+; TODO: using cursors, this is now much slower; improve the performance?
 (define (step term)
-  (match term
-    ((value _) (left (format "cannot step irreducible term: ~v" term)))
-    ((produce tm-0)
-     (do either-monad
-       tm-1 <- (step tm-0)
-       (pure (produce tm-1))))
-    ((subst sub tm) (right (substitute-explicit sub tm)))
-    ((action-2 act (value v0) (value v1))
-     (maybe->either (format "cannot step stuck term: ~v" term)
-                    (execute-action-2-explicit act v0 v1)))
-    ((action-2 act (value val) t1-0)
-     (do either-monad
-       t1-1 <- (step t1-0)
-       (pure (action-2 act (value val) t1-1))))
-    ((action-2 act t0-0 t1)
-     (do either-monad
-       t0-1 <- (step t0-0)
-       (pure (action-2 act t0-1 t1))))))
+  (begin/with-monad
+    either-monad
+    cterm <- (maybe->either (format "cannot step irreducible term: ~v" term)
+                            (step-continuation (::0 term)))
+    cterm <- (step-execute cterm)
+    (pure (::^*. cterm))))
 
 (define (step-safe term)
   (if (or (value? term) (produce? term) (subst? term) (action-2? term))
