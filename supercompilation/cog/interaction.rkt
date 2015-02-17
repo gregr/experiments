fa2d571bd968c8c7fd8cc1e2e53d2f322f7ed8ec
gregr <greg.weiqi@gmail.com>
2015-02-17 15:33:15 -0500

    Display interaction terms using markout

--- supercompilation/cog/interaction.rkt
+++ supercompilation/cog/interaction.rkt
@@ -11,12 +11,14 @@
   gregr-misc/cursor
   gregr-misc/either
   gregr-misc/list
+  gregr-misc/markout
   gregr-misc/match
   gregr-misc/maybe
   gregr-misc/monad
   gregr-misc/navigator
   gregr-misc/record
   gregr-misc/sugar
+  gregr-misc/terminal
   )
 
 (def (subst-keys (substitution uses _))
@@ -94,13 +96,143 @@
           "")
     "\n================================\n\n"))
 
+; TODO: style marks for position tracking
+(record style-palette
+  default
+  unit
+  bit
+  bvar
+  lam
+  lam-bracket
+  pair-bracket
+  pair-separator
+  subst-lift
+  subst-use
+  subst-separator
+  subst-bracket
+  produce
+  produce-bracket
+  access-bracket
+  apply-bracket
+  )
+(define style-palette-empty
+  (style-palette style-empty style-empty style-empty style-empty style-empty
+    style-empty style-empty style-empty style-empty style-empty style-empty
+    style-empty style-empty style-empty style-empty style-empty))
+
+(def (style-palette->doc-renderer
+       render-other
+       (style-palette
+         style-default
+         style-unit
+         style-bit
+         style-bvar
+         style-lam
+         style-lam-bracket
+         style-pair-bracket
+         style-pair-separator
+         style-subst-lift
+         style-subst-use
+         style-subst-separator
+         style-subst-bracket
+         style-produce
+         style-produce-bracket
+         style-access-bracket
+         style-apply-bracket
+         ))
+  pair-prefix = (doc-atom style-pair-bracket "{")
+  pair-suffix = (doc-atom style-pair-bracket "}")
+  pair-separator = (doc-atom style-pair-separator ",")
+  lam-prefix = (doc-atom style-lam-bracket "(")
+  lam-suffix = (doc-atom style-lam-bracket ")")
+  lam-doc = (doc-atom style-lam "λ")
+  unit-doc = (doc-atom style-unit "{}")
+  b-0-doc = (doc-atom style-bit "0")
+  b-1-doc = (doc-atom style-bit "1")
+  subst-prefix = (doc-atom style-subst-bracket "[")
+  subst-suffix = (doc-atom style-subst-bracket "]")
+  subst-separator = (doc-atom style-subst-separator ",")
+  produce-prefix = (doc-atom style-produce-bracket "(")
+  produce-suffix = (doc-atom style-produce-bracket ")")
+  produce-doc = (doc-atom style-produce "produce")
+  access-prefix = (doc-atom style-access-bracket "[")
+  access-suffix = (doc-atom style-access-bracket "]")
+  apply-prefix = (doc-atom style-apply-bracket "(")
+  apply-suffix = (doc-atom style-apply-bracket ")")
+
+  (letrec ((render
+    (fn (t/v)
+      (match t/v
+        ((uno)       unit-doc)
+        ((bit (b-0)) b-0-doc)
+        ((bit (b-1)) b-1-doc)
+        ((pair l r)
+         (lets
+           items = (map render (list l r))
+           items = (separated pair-separator style-default items)
+           (bracketed-chain pair-prefix pair-suffix attr-loose-aligned
+                            style-default style-default items)))
+        ((bvar idx) (doc-atom style-bvar (format "$~a" idx)))
+        ((lam attr body)
+         (bracketed-chain lam-prefix lam-suffix attr-loose-aligned
+                          style-default style-default
+                          (list lam-doc (render body))))
+        ((subst (substitution uses lift) t)
+         (lets
+           bindings = (map render (map substitution-use-v uses))
+           lift = (doc-atom style-subst-lift (format "^~a" lift))
+           bindings = (separated subst-separator style-default
+                                 (list* lift bindings))
+           sub = (bracketed-chain subst-prefix subst-suffix attr-loose-aligned
+                                  style-default style-default bindings)
+           body = (render t)
+           (tight-pair style-default sub body)))
+        ((value v) (render v))
+        ((produce t)
+         (bracketed-chain produce-prefix produce-suffix attr-loose-aligned
+                          style-default style-default
+                          (list produce-doc (render t))))
+        ((pair-access index pair)
+         (lets
+           index =
+           (bracketed-chain access-prefix access-suffix attr-loose-aligned
+                            style-default style-default
+                            (list (render index)))
+           (tight-pair style-default (render pair) index)))
+        ((lam-apply proc arg)
+         (bracketed-chain apply-prefix apply-suffix attr-loose-aligned
+                          style-default style-default
+                          (map render (list proc arg))))
+        (x (render-other x))))))
+    render))
+(define doc-render-empty
+  (style-palette->doc-renderer (void) style-palette-empty))
+
+(def (interact-context->doc nav)
+  doc-empty = (doc-atom style-empty "")
+  parts =
+  (forl
+    (list focus hole-pos) <- (navigator-path nav)
+    (doc-render-empty focus))
+  parts = (add-between parts doc-empty)
+  (vertical-list style-empty parts))
+
+(def (doc-show doc)
+  (size width height) = (screen-size)
+  ctx = (sizing-context-new-default)
+  block = (doc->styled-block ctx style-empty (min 80 width) doc)
+  block-str = (styled-block->string block)
+  (string-append block-str "\n"))
+
+(define view-syntax-doc
+  (compose1 doc-show interact-context->doc))
 (define view-syntax-raw
   (compose1 chain-show interact-context-present))
 (define view-syntax-0
   (compose1 chain-show chain-unparse-void interact-context-present))
 (define (view-toggle current-view)
-  (right (if (eq? view-syntax-raw current-view)
-           view-syntax-0 view-syntax-raw)))
+  (right (if (eq? view-syntax-doc current-view)
+           view-syntax-0 view-syntax-doc)))
 
 (record interact-state view context history)
 (def (interact-state-viewcontext (interact-state view ctx _)) (view ctx))
