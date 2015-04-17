6f3c12a5bf9754cf87fb86c02734f5054aaaf615
gregr <greg.weiqi@gmail.com>
2015-04-17 16:45:30 -0400

    Define interaction->commands

--- supercompilation/cog/workspace-model.rkt
+++ supercompilation/cog/workspace-model.rkt
@@ -13,12 +13,39 @@
 
 ;workspace->doc ; workspace -> db -> doc
 
-; TODO
-(define (interaction->commands interaction) (void))
+(records interaction-command
+  (interaction-traverse-up name count)
+  (interaction-traverse-down name count)
+  (interaction-traverse-left name count)
+  (interaction-traverse-right name count)
+  (interaction-substitute-complete name)
+  (interaction-step name count)
+  (interaction-step-complete name)
+  (interaction-toggle-syntax name)
+  (interaction-undo name count)
+  (interaction-quit name))
+(define ((interaction->commands name) interaction)
+  ; TODO: specialized commands based on interaction state
+  `((#\h "traverse left"
+     ,(lambda (count) (interaction-traverse-left name count)))
+    (#\j "traverse down"
+     ,(lambda (count) (interaction-traverse-down name count)))
+    (#\k "traverse up"
+     ,(lambda (count) (interaction-traverse-up name count)))
+    (#\l "traverse right"
+     ,(lambda (count) (interaction-traverse-right name count)))
+    (#\S "substitute completely"
+     ,(lambda (count) (interaction-substitute-complete name)))
+    (#\s "step" ,(lambda (count) (interaction-step name count)))
+    (#\c "step completely" ,(lambda (count) (interaction-step-complete name)))
+    (#\x "toggle-syntax" ,(lambda (count) (interaction-toggle-syntax name)))
+    (#\u "undo" ,(lambda (count) (interaction-undo name count)))
+    (#\q "quit" ,(lambda (count) (interaction-quit name)))))
+
 (define (interaction->doc interaction) (void))
 
 (define (interaction-widget name)
   (define ((with-interaction f) db)
     (f (:.* db 'interactions name)))
-  (widget (with-interaction interaction->commands)
-          (with-interaction interaction->doc)))
+  (widget (with-interaction (interaction->commands name))
+          (with-interaction (interaction->doc name))))
