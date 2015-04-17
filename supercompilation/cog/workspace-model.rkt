119a7ae901cc60c1c9f886a6d1d615431e0d23de
gregr <greg.weiqi@gmail.com>
2015-04-17 14:53:31 -0400

    Describe interaction-pane

--- supercompilation/cog/workspace-model.rkt
+++ supercompilation/cog/workspace-model.rkt
@@ -9,8 +9,18 @@
 (record database workspaces interactions) ; {workspaces: {name => workspace}, interactions: {name => interaction}, ...}
 (record workspace panel panes) ; {panel: panel, panes: {name => pane}}
 (record panel layout focus-index notification) ; {layout: [name], focus-index: nat, notification: string}
-(record pane db->commands db->doc) ; interaction-panes close over an interaction-name
+(record pane db->commands db->doc)
 
 ;workspace->doc ; workspace -> db -> doc; uses panel-focus->commands
   ;panel-focus->commands ; panel -> panes -> db -> commands
 ;panel->commands ; panel -> commands
+
+; TODO
+(define (interaction->commands interaction) (void))
+(define (interaction->doc interaction) (void))
+
+(define (interaction-pane name)
+  (define ((with-interaction f) db)
+    (f (:.* db 'interactions name)))
+  (pane (with-interaction interaction->commands)
+        (with-interaction interaction->doc)))
