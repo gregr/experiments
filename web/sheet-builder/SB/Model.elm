4c20ef4f1950405ace363787f11de074dcdeca87
gregr <greg.weiqi@gmail.com>
2016-04-15 16:42:19 -0400

    Start defining eval

--- web/sheet-builder/SB/Model.elm
+++ web/sheet-builder/SB/Model.elm
@@ -9,15 +9,36 @@ type TermT ref
   | TSheet (Sheet ref)
   | Identify (Identifier ref)
   | Access (Accessor ref)
---  | Apply
+  | UnaryOp UOp (TermT ref)
+  | BinaryOp BOp (TermT ref) (TermT ref)
+  --| Apply
+type UOp
+  = UNot
+  | UAddInvert
+  | URound
+  | UFloor
+  | UCeiling
+type BOp
+  = BCompareEQ
+  | BCompareLT
+  | BCompareLTE
+  | BCompareGT
+  | BCompareGTE
+  | BAdd
+  | BSubtract
+  | BMultiply
+  | BDivide
+  | BStringConcat
+  | BStringSubrange
+  | BStringReplace
 type Atom ref
   = ARef ref
-  | AUnit ()
+  | AUnit
   | ABool Bool
   | AString String
-  | ANumber Number
-type Number = NInt Int | NFloat Float
-type alias Accessor expr = { collection : expr, key : expr }
+  | AInt Int
+  | AFloat Float
+type alias Accessor ref = { collection : ref, key : Atom ref }
 type alias Identifier ref = { namespace : ref, nref : NamedRef ref }
 type alias NamedRef ref = { name : Name, ref : ref }
 
@@ -47,11 +68,34 @@ type alias FlatTerm = TermT Ref
 -- deletion, (partial)transferring dependencies
 
 type alias Level = Int
-type IRef = IAbsRef Ref | IRelRef (Level, Ref)
-type IterTerm = ITerm (TermT IRef) | IPos Level
+type IRef = IAbsRef Ref | IRelRef (Level, Ref) | IPos Level
+type alias IterTerm = TermT IRef
 
 type alias Iteration ref =
   { body : IterTerm
   , locals : List IterTerm
   , length : ref
   }
+
+afloat atom = case atom of
+  AInt int -> toFloat int
+  AFloat float -> float
+  -- ARef ref -> -- TODO: recurse on referenced value
+  _ -> 0  -- TODO: Nothing
+
+eval term = case term of
+  Literal atom -> atom
+  BinaryOp op lhs rhs ->
+    let vlhs = afloat <| eval lhs
+        vrhs = afloat <| eval rhs
+        vres = case op of
+          BAdd -> vlhs + vrhs
+          BSubtract -> vlhs - vrhs
+          BMultiply -> vlhs * vrhs
+          BDivide -> vlhs / vrhs
+          _ -> 0  -- TODO
+    in AFloat vres
+  _ -> AUnit
+
+example = BinaryOp BMultiply (Literal <| AFloat 4.1) (Literal <| AInt 3)
+test = eval example
