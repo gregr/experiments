70de8ace67bb94aeef1b6c9e6f2a49b054105ee7
gregr <greg.weiqi@gmail.com>
2016-04-21 11:20:00 -0400

    Add Main.elm

---
+++ web/sheet-builder/SB/Main.elm
@@ -0,0 +1,8 @@
+import SB.Model as M
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy, lazy2, lazy3)
+import Json.Decode as J
+
+main = text <| toString M.test
